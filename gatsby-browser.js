bb9ebbb40732cc3e70244f2a43a7c9b3bbdcaf7a
Caleb Flanagan <caleb.flanagan@clearlink.com>
2024-06-17 12:40:32 -0600

    pulling out emotion.js stuff to start over

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,7 @@ import React from "react"
 import { CacheProvider } from "@emotion/react"
 import createCache from "@emotion/cache"
 import { cache as emotionCache } from "@emotion/css"
+import "normalize.css"
 
 const cache = createCache({ key: "cuttlemaster" })
 
--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,7 @@
 import React from "react"
-import { css } from "@emotion/react"
-
-const headerStyle = css`
-  background-color: #333;
-  color: white;
-  padding: 1rem;
-  text-align: center;
-  text-transform: uppercase;
-`
 
 const Header = () => (
-  <header css={headerStyle}>
+  <header>
     <h1>Cuttlemaster</h1>
   </header>
 )
