dd86331996d6a707627da40f21fd61a5556a1a19
Caleb Flanagan <caleb.flanagan@clearlink.com>
2024-06-20 17:30:33 -0600

    removing contentful connections to try again later

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+import MainContainer from "../components/MainContainer"
+
+const NotFoundPage = () => (
+  <MainContainer>
+    <h1>404: Not Found</h1>
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  </MainContainer>
+)
+
+// export const Head = () => <Seo title="404: Not Found" />
+
+export default NotFoundPage
