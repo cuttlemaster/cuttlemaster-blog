8763b43eb0d5514d9b69e38caf50fff9e0db3bc8
Caleb Flanagan <caleb.flanagan@clearlink.com>
2024-07-11 14:25:47 -0600

    fixing more mobile styles

--- src/components/Post.js
+++ src/components/Post.js
@@ -16,25 +16,36 @@ const PostHeader = styled.div`
     position: absolute;
     top: 0;
     left: 0;
-    padding: 4px 15px 3px 15px;
+    padding: 3px 10px 3px 10px;
     background-color: ${theme.colors.gray};
     color: ${theme.colors.htmlBG};
     font-family: ${theme.fonts.Rubik};
-    font-size: 14px;
-    line-height: 14px;
+    font-size: 10px;
+    line-height: 10px;
     text-transform: uppercase;
+
+    @media (min-width: 768px) {
+      padding: 4px 15px 3px 15px;
+      font-size: 14px;
+      line-height: 14px;
+    }
   }
 `
 
 const CoverImage = styled.img`
   float: left;
   margin-top: 6px;
-  margin-right: 25px;
+  margin-right: 15px;
   margin-bottom: 20px;
-  max-width: 200px;
+  max-width: 100px;
   width: 100%; /* ensures the image respects the max-width */
   height: auto; /* ensures the image maintains aspect ratio */
   box-shadow: 4px 4px 6px rgba(0, 0, 0, 0.3);
+
+  @media (min-width: 768px) {
+    margin-right: 25px;
+    max-width: 200px;
+  }
 `
 
 const Post = ({ title, date, coverArtURL, content }) => (
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -15,21 +15,35 @@ const GlobalStyles = () => {
         }
 
         h2 {
-          margin-bottom: 15px;
-          padding: 5px 0 5px 0;
+          margin-bottom: 5px;
+          padding: 0 0 0 0;
           color: ${theme.colors.gray};
           font-family: ${theme.fonts.Rubik};
-          font-size: 36px;
-          line-height: 42px;
+          font-size: 26px;
+          line-height: 30px;
+
+          @media (min-width: 768px) {
+            margin-bottom: 15px;
+            padding: 5px 0 5px 0;
+            font-size: 36px;
+            line-height: 42px;
+          }
         }
 
         p {
-          margin-bottom: 25px;
+          margin-bottom: 15px;
           color: ${theme.colors.gray};
           font-family: ${theme.fonts.PTSerif};
-          font-size: 20px;
-          line-height: 30px;
-          text-align: justify;
+          font-size: 14px;
+          line-height: 20px;
+          text-align: left;
+
+          @media (min-width: 768px) {
+            margin-bottom: 25px;
+            font-size: 20px;
+            line-height: 30px;
+            text-align: justify;
+          }
         }
       `}
     />
